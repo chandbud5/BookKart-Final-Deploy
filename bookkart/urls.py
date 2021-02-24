949a1ad529164c973ed244f26c1bf7f859e12189
chandbud5 <chandbud5@gmail.com>
2021-02-24 14:03:28 +0530

    urls update

--- bookkart/urls.py
+++ bookkart/urls.py
@@ -4,4 +4,4 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('shop.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
