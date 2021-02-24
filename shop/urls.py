151261f5d91522868196e7d5099db22d96195bc9
chandbud5 <chandbud5@gmail.com>
2021-02-24 14:02:17 +0530

    urls

--- shop/urls.py
+++ shop/urls.py
@@ -9,3 +9,4 @@ urlpatterns = [
     path('signup/', views.signup, name="signup"),
     path('register/', views.register, name="register"),
 ]
+urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
